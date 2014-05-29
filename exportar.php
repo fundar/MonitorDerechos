f530f67de77b5dc33f64415b1a903dc94a390c3f
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-05-29 12:24:09 -0500

    metodo para exportar

---
+++ exportar.php
@@ -0,0 +1,28 @@
+<?php
+$date     = date_create();
+$filename = 'dump-' . date_timestamp_get($date) . '.sql';
+
+//ENTER THE RELEVANT INFO BELOW
+$mysqlDatabaseName ='ddhhdb';
+$mysqlUserName     ='root';
+$mysqlPassword     ='root';
+$mysqlHostName     ='localhost';
+$mysqlExportPath   ='database/exports/' . $filename;
+
+//DO NOT EDIT BELOW THIS LINE
+//Export the database and output the status to the page
+$command ='mysqldump --opt -h' .$mysqlHostName .' -u' .$mysqlUserName .' -p' .$mysqlPassword .' ' .$mysqlDatabaseName .' > ' .$mysqlExportPath;
+exec($command, $output=array(), $worked);
+
+switch($worked) {
+	case 0:
+		echo 'Database <b>' .$mysqlDatabaseName .'</b> successfully exported to <b>~/' .$mysqlExportPath .'</b>';
+		header('Location: ' . $mysqlExportPath);
+	break;
+	case 1:
+		echo 'There was a warning during the export of <b>' .$mysqlDatabaseName .'</b> to <b>~/' .$mysqlExportPath .'</b>';
+	break;
+	case 2:
+		echo 'There was an error during export. Please check your values:<br/><br/><table><tr><td>MySQL Database Name:</td><td><b>' .$mysqlDatabaseName .'</b></td></tr><tr><td>MySQL User Name:</td><td><b>' .$mysqlUserName .'</b></td></tr><tr><td>MySQL Password:</td><td><b>NOTSHOWN</b></td></tr><tr><td>MySQL Host Name:</td><td><b>' .$mysqlHostName .'</b></td></tr></table>';
+	break;
+}
