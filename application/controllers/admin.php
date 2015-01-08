1786bf08f912e8f510f828e60a8c95c05a7cd520
Kevin Martinez <n4utilius@gmail.com>
2015-01-08 16:33:29 -0600

    agregar columna test

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -373,7 +373,7 @@ class Admin extends CI_Controller {
 
 
 		/*Columnas(Vista), campos y campos obligatorios*/
-		$crud->columns('id_migrante', 'id_lugar_denuncia', 'nombre', 'id_pais', 'id_estado', 'municipio', 'edad');
+		$crud->columns('id_migrante', 'id_lugar_denuncia', 'nombre', 'id_pais', 'id_estado', 'municipio', 'edad', 'denuncia');
 		
 		$crud->required_fields('nombre');
 
