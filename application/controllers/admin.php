cbc19efec2ff38e19b558fb5cca8b41d0ad784c0
Kevin Martinez <n4utilius@gmail.com>
2015-01-08 16:30:07 -0600

    agregar columna test

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -365,7 +365,7 @@ class Admin extends CI_Controller {
 		$crud->field_type('espanol', 'dropdown', array(1 => 'Si', 2 => 'No'));
 		
 		/*Denuncia*/
-		$crud->set_relation_n_n('denuncia', 'migrantes2denuncias','denuncias', 'id_migrante', 'id_denuncia', 'denuncias.id_denuncia');
+		$crud->set_relation_n_n('denuncia', 'migrantes2denuncias','denuncias', 'id_migrante', 'id_denuncia', '.id_denuncia');
 		$crud->add_action('Denuncia', '', '','.algo',array($this,'link_denuncia'));
 
 		$crud->unset_columns('Denuncia');
