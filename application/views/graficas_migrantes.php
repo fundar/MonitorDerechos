d94df8be06f1ddc5e96dcc71d08c5778f1449c27
Kevin Martinez <n4utilius@gmail.com>
2015-01-12 17:17:20 -0600

    Vista para gráficas de migrantes

--- application/views/graficas_migrantes.php
+++ application/views/graficas_migrantes.php
@@ -1,4 +1,9 @@
 <?php foreach($query as $row){ ?>
 	<li><?php echo $row->nombre; ?></li>
 <?php }?>
-sdfk
+
+<script type="text/javascript">
+	
+	var data = "<?php echo json_encode($query);?>";
+	console.log(data);
+</script>
