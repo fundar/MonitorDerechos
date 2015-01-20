70f14821cc20dcf03c5daef03bdefae0552db6d2
Kevin Martinez <n4utilius@gmail.com>
2015-01-20 13:49:27 -0600

    rango de fechas en graficas

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -605,7 +605,8 @@ class Admin extends CI_Controller {
 		$this->load->model('migracion_model');
 
 		$start = $this->input->post("start") or "01/01/2014";
-		$end = $this->input->post("end") or "31/12/2014";
+		$start = "2014-01-01";
+		$end = "2014-12-01";
         
         $data['denuncias'] = $this->migracion_model->allDenuncias($start, $end);
 		$this->load->view('graficas_migrantes.php', $data);
