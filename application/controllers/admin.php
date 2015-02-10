4aa6237e61f104d590ac5981132aa908f8b81b0d
Kevin Martinez <n4utilius@gmail.com>
2015-02-10 17:39:55 -0600

    test

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -431,7 +431,7 @@ class Admin extends CI_Controller {
     	$state_info = $crud->getStateInfo();
 		//$crud->callback_after_insert(array($this, 'log_user_after_insert'));
 		if($state == 'insert') {
-	        $crud->callback_after_insert(function () {
+	        $crud->callback_after_insert(function ($post_array, $primary_key ) {
 	        	$data = array('a' => 1, 'b' => 2, 'ID' => $primary_key );    
 			    header('Content-Type: application/json');
 			    echo json_encode( $data );
